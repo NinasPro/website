103c3462bf33448fb3d94f00c12b94580c08a0da
Jazmine Maldonado <jazmine@imfd.com>
2020-05-02 21:43:08 -0400

    Add HomePage with carrousel and three image button items

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="about">
+    <h1>Somos</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "About",
+  components: {
+  }
+};
+</script>
---
+++ src/views/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="projects">
+    <h1>Proyectos</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Projects",
+  components: {
+  }
+};
+</script>
