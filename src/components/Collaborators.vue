1c23160bd80c579125882e7803259fe1a8fa4723
Mariana <mdtorres1@miuandes.cl>
2021-01-14 10:30:08 -0300

    rewrite data collaborators

--- src/components/Collaborators.vue
+++ src/components/Collaborators.vue
@@ -2,47 +2,10 @@
   
   <div>
     <div class="columns is-multiline is-mobile" >
-      <div class="column is-3">
-          <a href="https://www.uandes.cl/">
-          <img  src="@/assets/logos/logo-uandes.jpg" width="350px" >
+      <div class="column is-3" v-for="item in data" :key="item.name">
+          <a :href="item.url">
+          <img  :src="getImgUrl(item.image)" width="350px" >
           </a>
-      </div>
-      <div class="column is-3">
-          <a href="http://ingenieria.uchile.cl/">
-          <img  src="@/assets/logos/logo-fcfm.png" width="350px">
-          </a>
-      </div>
-      <div class="column is-3">
-          <a href="https://www.udec.cl/pexterno/">
-          <img  src="@/assets/logos/logo-udec.png" width="350px" >
-          </a>
-      </div>
-      <div class="column is-3">
-          <a href="https://imfd.cl/">
-          <img  src="@/assets/logos/logo-imfd.png" width="350px" >
-          </a>
-      </div>
-      <div class="column is-3">
-          <a href="https://technovation.cl/">
-          <img  src="@/assets/logos/logo-technovation.jpg" width="350px">
-          </a>
-      </div>
-      <div class="column is-3">
-          <a href="https://www.olimpiada-informatica.cl/">
-          <img  src="@/assets/logos/logo-oci.png" width="350px" >
-          </a>
-      </div>
-      <div class="column is-3">
-          <a href="https://fintual.cl/">
-          <img  src="@/assets/logos/logo-fintual.png" width="350px">
-          </a>
-      </div>
-      <div class="column is-3">
-   
-          <a href="https://cornershopapp.com/es-cl/">
-          <img  src="@/assets/logos/logo_cornershop.png" width="250px" >
-          </a>
-    
       </div>
       
     </div>
@@ -53,14 +16,19 @@
 
 
 <script>
-
+import * as Partners from '../data/collaborators.js';
 export default {
   name: 'Demo',
-
+  
   data () {
-
+    const data = Partners.default.data
     return {
-      
+      data,
+    }
+  },
+  methods: {
+    getImgUrl(value) {
+        return require(`@/assets/${value}`)
     }
   }
   
@@ -68,8 +36,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-
-
-</style>
