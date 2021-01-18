a65ac33cc113eeea5792fc4a54a53a2efd3e292a
crobles <cinthya.robles@ug.uchile.cl>
2021-01-18 11:07:32 -0300

    modified style to match wireframe and removed the imagebutton component from home

--- src/components/ProjectsHome.vue
+++ src/components/ProjectsHome.vue
@@ -2,14 +2,14 @@
   <div class="container">
     <div class="card-image">
       <figure>
-        <img src="https://picsum.photos/350" alt="Placeholder image">
+        <img :src="getImgUrl(image)" alt="Placeholder image">
       </figure>
     </div>
     <div class="content">
       <p class="title"> {{title}} </p>
-      <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p class="description">{{description}}</p>
       <div class="morebutton">
-        <router-link :to="path" :class="`button ${type} is-rounded is-small`">
+        <router-link :to="path" :class="`button ${type} is-rounded is-medium`">
           Ver Más
         </router-link>
       </div>
@@ -21,20 +21,20 @@
 export default {
   props: {
     "type": String,
+    "image": String,
     "title": String,
     "text": String,
+    "description": String,
     "path": String,
-  },
-  data() {
-    return {
-      moreinfo: "url del sitio",
-    }
   },
   methods: {
     clickMe() {
       this.$buefy.notification.open('Clicked!!')
-    }
-  }
+    },
+    getImgUrl(value) {
+        return require(`@/assets/${value}`)
+    },
+  },
 }
 </script>
 
@@ -61,5 +61,8 @@ figure {
   padding-top: 20px;
   padding-bottom: 10px;
   margin-left: 20px;
+}
+.morebutton{
+  font-weight: bold;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,22 @@
   <div id="home">
     <Carrousel :type="type"/>
     <div class="container">
-      <div class="columns">
+      <div class="columns is-variable is-1">
         <div class="column">
-          <ImageButton type="is-info" image="lamarr.jpg" text="Somos" path="/somos/"/>
+          <ProjectsHome title="Empodera" type="is-danger" image="laschicaspueden.jpg" path="/somos/"
+          description="Curso Anual de Programación"/>
         </div>
         <div class="column">
-          <ImageButton type="is-warning" image="ninastic.jpg" text="Proyectos" path="/proyectos/"/>
+          <ProjectsHome title="Inspira" type="is-info" image="lamarr.jpg" path="/somos/"
+          description="Charla de Mujeres Pro"/>
         </div>
         <div class="column">
-          <ImageButton type="is-success" image="sororidad.jpg" text="¿Cómo participar?" path="/voluntariado/"/>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="columns">
-        <div class="column">
-          <ProjectsHome title="Proyecto 1" type="is-info" image="lamarr.jpg" path="/somos/"/>
+          <ProjectsHome title="Incentiva" type="is-warning" image="ninastic.jpg" path="/proyectos/"
+          description="Talleres Introductorios de Programación"/>
         </div>
         <div class="column">
-          <ProjectsHome title="Proyecto 2" type="is-warning" image="ninastic.jpg" path="/proyectos/"/>
-        </div>
-        <div class="column">
-          <ProjectsHome title="Proyecto 3" type="is-success" image="sororidad.jpg" path="/voluntariado/"/>
+          <ProjectsHome title="Potencia" type="is-success" image="sororidad.jpg" path="/voluntariado/"
+          description="Cursos Avanzados de Programación"/>
         </div>
       </div>
     </div>
@@ -32,14 +26,12 @@
 
 <script>
 import Carrousel from "../components/Carrousel.vue";
-import ImageButton from "../components/ImageButton.vue";
 import ProjectsHome from "../components/ProjectsHome.vue";
 
 export default {
   name: "Home",
   components: {
     Carrousel,
-    ImageButton,
     ProjectsHome,
   },
   props: {
