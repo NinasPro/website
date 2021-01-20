e2416f38a11796708f9e45f7e5045cd8738a1acd
Mariana <mdtorres1@miuandes.cl>
2021-01-20 16:46:23 -0300

    add button involucrate y dona

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,16 +10,23 @@
     </template>
     <template slot="end">
       <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        Inicio
+        Iniciativas
       </b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/somos/' }">
-        Somos
+        Eventos
       </b-navbar-item>
       <b-navbar-item tag="router-link" :to="{ path: '/proyectos/' }">
-        Proyectos
+        Somos
       </b-navbar-item>
-      <b-navbar-item tag="router-link" :to="{ path: '/voluntariado/' }">
-        ¿Cómo participar?
+      <b-navbar-item   tag="router-link" style="padding: 0px 20px;" :to="{ path: '/voluntariado/' }">
+        <button class="button is-info" style="margin-top:40px; margin:0px 0px;">
+          Involucrate
+        </button>
+      </b-navbar-item>
+      <b-navbar-item   tag="router-link" style="padding: 0px 20px;" :to="{ path: '/voluntariado/' }">
+        <button class="button is-donation" style="margin-top:40px; width: 50px; margin:0px 0px;" >
+          Dona
+        </button>
       </b-navbar-item>
     </template>
   </b-navbar>
@@ -40,12 +47,14 @@ export default {
   font-size: 20px;
   font-weight: bold;
   padding: 0px 40px;
-
+  
   img {
     height: 7rem;
     margin-top: 1rem;
     max-height: 10rem;
+    
   }
 
 }
+
 </style>
