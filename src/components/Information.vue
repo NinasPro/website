d86e983177710abae7b9b271f0796a325bd998dc
Mariana <mdtorres1@miuandes.cl>
2021-01-25 16:58:44 -0300

    fix text margin

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -10,7 +10,7 @@
                         <div v-for="code in text.split('<br>')" :key=code>{{code}} </div>
                     </h1>
                     <router-link :to="Path">
-                    <button class="button-information button is-info " style=" margin-inline-start: 192px;"> {{boton}} </button>
+                    <button class="button-information button is-info " style=" margin-inline-start: 23.1%;"> {{boton}} </button>
                     </router-link>
                 </div>
                 </div>
@@ -24,7 +24,7 @@
                         <div v-for="code in text.split('<br>')" :key=code>{{code}} </div>
                     </h1>
                      <router-link :to="Path">
-                        <button class="button-information button is-success " style="margin-inline-start: 385.226px; " > {{boton}} </button>
+                        <button class="button-information button is-success " style="margin-inline-start: 25%;" > {{boton}} </button>
                      </router-link> 
                 </div>
                 <div class="column">
@@ -68,44 +68,46 @@ export default {
 <style>
     .right{
         border-radius: 1000px 0px 0px 1000px;
-        width: 1300px;
-        height: 450px;
+        width: 100%;
+        height: 75%;
         object-fit: cover;
         margin: 2rem 0rem 2rem 20rem;
     }
-    .style-title-right{
-        font-size: 2.5em; 
-        padding: 120px 2px 0px; 
-        line-height: 1.5; 
-        margin-left: 192px; 
-    }
-    .style-text-right{
-        font-size: 1.65em; 
-        padding: 20px 2px ; 
-        line-height: 1.5; 
-        margin-inline-end: 385.226px; 
-        margin-inline-start: 192px;
-    }
-
-    .left{
-        border-radius: 0px 1000px 1000px 0px;
-        width: 1300px;
-        height: 450px;
-        object-fit: cover;
-        margin: 2rem 0rem 2rem 0rem;
-    } 
     .style-title-left{
         font-size: 2.5em; 
-        margin-inline-start: 385.226px; 
-        margin-top:120px; 
+        margin-inline-start: 25%; 
+        margin-top:14.5%; 
         line-height: 1.5;
     }
     .style-text-left{
         font-size: 1.65em; 
-        padding: 20px 2px ; 
-        margin-inline-start: 385.226px; 
-        margin-inline-end: 120px; 
+        padding: 1% 0.5% ; 
+        margin-inline-start: 25%; 
+        margin-inline-end: 14.5%; 
         line-height: 1.5;
     }
+    
+    .left{
+        border-radius: 0px 1000px 1000px 0px;
+        width: 100%;
+        height: 75%;
+        object-fit: cover;
+        margin: 2rem 0rem 2rem 0rem;
+    } 
+    .style-title-right{
+        font-size: 2.5em; 
+        padding: 15% 0.2% 0%; 
+        line-height: 1.5; 
+        margin-inline-end: 25%; 
+        margin-inline-start: 23.1%;
+    }
+    .style-text-right{
+        font-size: 1.65em; 
+        padding: 1% 0.5% ; 
+        line-height: 1.5; 
+        margin-inline-end: 25%; 
+        margin-inline-start: 23.1%;
+    }
+    
     
 </style>
