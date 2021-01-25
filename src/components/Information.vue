004656c8a6712fcdb4fdd9a7f0ed7cd62c98232d
Jazmine Maldonado <jazmine@imfd.com>
2021-01-25 15:23:17 -0300

    Fix indentation

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,68 +1,61 @@
 <template >
-            <div v-if="type === 'left'">
-                <div class="columns">
-                <div class="column">
-                <img :class="type" :src="getImgUrl(image)" :align="type"></div>
-                <div class="column is-one-quarte"> 
-                    <h1 class="title style-title-right" align="left" > {{titulo}}
-                    </h1>
-                    <h1 class =" style-text-right"  align="left" > 
-                        <div v-for="code in text.split('<br>')" :key=code>{{code}} </div>
-                    </h1>
-                    <router-link :to="Path">
+    <div v-if="type === 'left'">
+        <div class="columns">
+            <div class="column">
+                <img :class="type" :src="getImgUrl(image)" :align="type">
+            </div>
+            <div class="column is-one-quarte"> 
+                <h1 class="title style-title-right" align="left"> {{titulo}} </h1>
+                <h1 class =" style-text-right"  align="left" > 
+                    <div v-for="code in text.split('<br>')" :key=code>{{code}} </div>
+                </h1>
+                <router-link :to="Path">
                     <button class="button-information button is-info " style=" margin-inline-start: 192px;"> {{boton}} </button>
-                    </router-link>
-                </div>
-                </div>
+                </router-link>
             </div>
-            <div v-else>
-                <div class="columns">
-                <div class="column is-one-quarte"> 
-                    <h1 class="title style-title-left" align="left"> {{titulo}}
-                    </h1>
-                    <h1 class =" style-text-left"  align="left" > 
-                        <div v-for="code in text.split('<br>')" :key=code>{{code}} </div>
-                    </h1>
-                     <router-link :to="Path">
-                        <button class="button-information button is-success " style="margin-inline-start: 385.226px; " > {{boton}} </button>
-                     </router-link> 
-                </div>
-                <div class="column">
-                <img :class="type" :src="getImgUrl(image)" :align="type"></div>
-            
+        </div>
+    </div>
+    <div v-else>
+        <div class="columns">
+            <div class="column is-one-quarte"> 
+                <h1 class="title style-title-left" align="left"> {{titulo}} </h1>
+                <h1 class =" style-text-left"  align="left"> 
+                    <div v-for="code in text.split('<br>')" :key=code> {{code}} </div>
+                </h1>
+                <router-link :to="Path">
+                    <button class="button-information button is-success" style="margin-inline-start: 385.226px;"> {{boton}} </button>
+                </router-link> 
             </div>
-        </div> 
-        
-  
-    
+            <div class="column">
+                <img :class="type" :src="getImgUrl(image)" :align="type">
+            </div>
+        </div>
+    </div>   
 </template>
 
 <script>
 
 export default {
-  props:
-  {
-      "type": String,
-      "image" : String,
-      "text" : String,
-      "boton" : String,
-      "format" :String,
-      "titulo":String,
-      "Path" : String,
-  },
-  
-  data () {
+    props:
+    {
+        "type": String,
+        "image" : String,
+        "text" : String,
+        "boton" : String,
+        "format" :String,
+        "titulo":String,
+        "Path" : String,
+    },
+    data () {
 
-  }
-  ,
-  methods: {
-    getImgUrl(value) {
-        return require(`@/assets/${value}`)
+    },
+    methods: {
+        getImgUrl(value) {
+            return require(`@/assets/${value}`)
+        }
     }
-  }
-  
-
 }
+
 </script>
 
 <style>
@@ -86,7 +79,6 @@ export default {
         margin-inline-end: 385.226px; 
         margin-inline-start: 192px;
     }
-
     .left{
         border-radius: 0px 1000px 1000px 0px;
         width: 1300px;
@@ -106,6 +98,5 @@ export default {
         margin-inline-start: 385.226px; 
         margin-inline-end: 120px; 
         line-height: 1.5;
-    }
-    
+    }    
 </style>
