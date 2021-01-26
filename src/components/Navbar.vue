5529b506742ca59cb39ec265f130cc3a84beb478
Mariana <mdtorres1@miuandes.cl>
2021-01-26 13:13:49 -0300

    change the color in about and project

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -30,12 +30,18 @@
         Somos
       </b-navbar-item>
       <b-navbar-item   tag="router-link" style="padding: 0px 20px;" :to="{ path: '/voluntariado/' }">
-        <button class="button is-info navbar-involucrate" >
+        <button class="button is-info navbar-involucrate" v-if="type !== 'is-info'">
+          Involucrate
+        </button>
+        <button class="button is-warning navbar-involucrate" v-if="type === 'is-info'">
           Involucrate
         </button>
       </b-navbar-item>
       <b-navbar-item   tag="router-link" style="padding: 0px 20px;" :to="{ path: '/voluntariado/' }">
-        <button class="button navbar-donation">
+        <button class="button navbar-donation" v-if="type !== 'is-warning' && type !== 'is-info'">
+          Dona
+        </button>
+        <button class="button navbar-donation-info" v-if="type !== 'is-warning' || type !== 'is-info'">
           Dona
         </button>
       </b-navbar-item>
@@ -76,6 +82,15 @@ export default {
   border-color: #fdca36;
   border-width: 5px; 
   color:#fdca36;
+}
+.navbar-donation-info{
+  margin-top:40px; 
+  width: 50px; 
+  margin:0px 0px; 
+  background: transparent;
+  border-color: #2b88f7;
+  border-width: 5px; 
+  color:#2b88f7;
 }
 
 .navbar-involucrate{
