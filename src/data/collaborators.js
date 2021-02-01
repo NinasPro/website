eda33723408d356f0ad0138a45bbd4f977456a53
Jazmine Maldonado <jazmine@imfd.com>
2021-02-01 17:00:53 -0300

    replace images

--- src/data/collaborators.js
+++ src/data/collaborators.js
@@ -1,13 +1,13 @@
 export default ({
     data : [
-        {'name':'uandes','image':'logos/logo-uandes.jpg', 'url':"https://www.uandes.cl/"},
-        {'name':'fcfm','image':'logos/logo-fcfm.jpg', 'url':"http://ingenieria.uchile.cl/"},
-        {'name':'udec','image':'logos/logo-udec.jpg', 'url':"https://www.udec.cl/pexterno/"},
-        {'name':'imfd','image':'logos/logo-imfd.jpg', 'url':"https://imfd.cl/"},
-        {'name':'technovation','image':'logos/logo-technovation.jpg', 'url':"https://technovation.cl/"},
-        {'name':'oci','image':'logos/logo-oci.jpg', 'url':"https://www.olimpiada-informatica.cl/"},
-        {'name':'fintual','image':'logos/logo-fintual.jpg', 'url':"https://fintual.cl/"},
-        {'name':'cornershop','image':'logos/logo-cornershop.jpg', 'url':"https://cornershopapp.com/es-cl/"},
+        {'name':'uandes','image':'logos/uandes.png', 'url':"https://www.uandes.cl/"},
+        {'name':'fcfm','image':'logos/fcfm.png', 'url':"http://ingenieria.uchile.cl/"},
+        {'name':'udec','image':'logos/udec.png', 'url':"https://www.udec.cl/pexterno/"},
+        {'name':'imfd','image':'logos/imfd.png', 'url':"https://imfd.cl/"},
+        {'name':'technovation','image':'logos/technovation.png', 'url':"https://technovation.cl/"},
+        {'name':'oci','image':'logos/oci.png', 'url':"https://www.olimpiada-informatica.cl/"},
+        {'name':'fintual','image':'logos/fintual.png', 'url':"https://fintual.cl/"},
+        {'name':'cornershop','image':'logos/cornershop.png', 'url':"https://cornershopapp.com/es-cl/"},
         
            ]
 })
