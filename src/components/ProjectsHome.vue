544e80da251402a2b3a29eb4ae3a452880ef73e5
Jazmine Maldonado <jazmine@imfd.com>
2021-02-11 23:03:37 -0300

    simplify style of component

--- src/components/ProjectsHome.vue
+++ src/components/ProjectsHome.vue
@@ -7,12 +7,12 @@
     </div>
     <div class="content">
       <p class="title"> {{title}} </p>
-      <p class="description">{{description}}</p>
-      <div class="morebutton">
-        <router-link :to="path" :class="`button ${type} is-rounded is-medium`">
-          Ver Más
-        </router-link>
-      </div>
+      <p class="description"> {{description}} </p>
+    </div>
+    <div class="card-end">
+      <router-link :to="path" :class="`button ${type} is-rounded`">
+        Ver Más
+      </router-link>
     </div>
   </div>
 </template>
@@ -28,9 +28,6 @@ export default {
     "path": String,
   },
   methods: {
-    clickMe() {
-      this.$buefy.notification.open('Clicked!!')
-    },
     getImgUrl(value) {
         return require(`@/assets/${value}`)
     },
@@ -38,34 +35,39 @@ export default {
 }
 </script>
 
-<style scoped>
-.card-image {
-  text-align: center;
-}
-.title {
-  text-align: left;
-  font-size: 16px;
-  margin-left: 5%;
-}
-.description {
-  text-align: left;
-  font-size: 18px;
-  margin-left: 5%;
-}
-.columns, .content{
-  text-align: center;
-  padding-left: 5%;
-}
-figure {
-  display: block;
-  padding-top: 10%;
-  padding-bottom: 5%;
-  padding-left: 8%;
-}
-.morebutton{
-  font-weight: bold;
-}
+<style lang="scss" scoped>
+
 .container {
-  padding-top: 70%;
+  margin: 12px;
+
+  .content {
+    padding: 5px;
+    min-height: 80px;
+    text-align: left;
+
+    .title {
+      font-size: 20px;
+      margin-bottom: 10px;
+    }
+
+  }
+
+  .card-end {
+    text-align: center;
+  }
+}
+
+@media only screen and (max-device-width: 1220px) {
+  .container {
+    margin: 40px;
+
+    .content {
+      min-height: 0px;
+
+      .title {
+        margin-bottom: 5px;
+      }
+    }
+  }
 }
 </style>
