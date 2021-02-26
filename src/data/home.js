c17612a11d71f33e484a116877b8a895bc8c1090
Mariana <mdtorres1@miuandes.cl>
2021-02-26 11:40:27 -0300

    change fo titles and content initiatives

--- src/data/home.js
+++ src/data/home.js
@@ -6,10 +6,10 @@ export default ({
             {'title': 'Curso Virtual 2021','subtitle': 'El 10 de Abril comenzará el curso anual de programación competitiva para niñas en modalidad online. ¡Postula para participar, escoge tu sede más cercana!','image': 'banner-img-3.png'}
         ],
         projectsHome:[
-            {'title':'Empodera','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación'},
-            {'title':'Inspira', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro ~texto de relleno~'},
-            {'title':'Incentiva', 'type':'is-warning' ,'image':'ninastic.jpg', 'path':'/incentiva/','description':'Talleres Introductorios de Programación'},
-            {'title':'Potencia', 'type':'is-success', 'image':'sororidad.jpg', 'path':'/potencia/', 'description':'Cursos Avanzados de Programación'},
+            {'title':'Empower Initiative','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación Competitiva en C++'},
+            {'title':'Initiative Inspires', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro y creación de Role Models'},
+            {'title':'Incentive Initiative', 'type':'is-warning' ,'image':'ninastic.jpg', 'path':'/incentiva/','description':'Talleres Introductorios de Programación'},
+            {'title':'Initiative Power', 'type':'is-success', 'image':'sororidad.jpg', 'path':'/potencia/', 'description':'Cursos Avanzados de Programación'},
         ],
         information:[
             {'type':'left', 'image':'inspired.jpg', 'text':'Si tienes menos de 18 años. Diviértete, aprende y conecta con otras Niñas Pro como tú.', 'titulo':'¡Participa!', 'boton':'Ver proximas actividades', 'Path':'/voluntariado/'},
@@ -37,10 +37,10 @@ export default ({
             {'title': 'Curso Virtual 2021','subtitle': 'El 10 de Abril comenzará el curso anual de programación competitiva para niñas en modalidad online. ¡Postula para participar, escoge tu sede más cercana!','image': 'banner-img-3.png'}
         ],
         projectsHome:[
-            {'title':'Empodera','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación'},
-            {'title':'Inspira', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro ~texto de relleno~'},
-            {'title':'Incentiva', 'type':'is-warning' ,'image':'ninastic.jpg', 'path':'/incentiva/','description':'Talleres Introductorios de Programación'},
-            {'title':'Potencia', 'type':'is-success', 'image':'sororidad.jpg', 'path':'/potencia/', 'description':'Cursos Avanzados de Programación'},
+            {'title':'Iniciativa Empodera','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación'},
+            {'title':'Iniciativa Inspira', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro ~texto de relleno~'},
+            {'title':'Iniciativa Incentiva', 'type':'is-warning' ,'image':'ninastic.jpg', 'path':'/incentiva/','description':'Talleres Introductorios de Programación'},
+            {'title':'Iniciativa Potencia', 'type':'is-success', 'image':'sororidad.jpg', 'path':'/potencia/', 'description':'Cursos Avanzados de Programación'},
         ],
         information:[
             {'type':'left', 'image':'inspired.jpg', 'text':'Si tienes menos de 18 años. Diviértete, aprende y conecta con otras Niñas Pro como tú.', 'titulo':'¡Participa!', 'boton':'Ver proximas actividades', 'Path':'/voluntariado/'},
