e48fafac6a917f53d6b008fcab38674a810986ba
Mariana <mdtorres1@miuandes.cl>
2021-02-26 11:43:41 -0300

    change information link

--- src/data/home.js
+++ src/data/home.js
@@ -37,14 +37,14 @@ export default ({
             {'title': 'Curso Virtual 2021','subtitle': 'El 10 de Abril comenzará el curso anual de programación competitiva para niñas en modalidad online. ¡Postula para participar, escoge tu sede más cercana!','image': 'banner-img-3.png'}
         ],
         projectsHome:[
-            {'title':'Iniciativa Empodera','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación'},
-            {'title':'Iniciativa Inspira', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro ~texto de relleno~'},
+            {'title':'Iniciativa Empodera','type':'is-danger','image':'laschicaspueden.jpg', 'path':'/empodera/','description':'Curso Anual de Programación Competitiva en C++'},
+            {'title':'Iniciativa Inspira', 'type':'is-info', 'image':'lamarr.jpg', 'path':'/inspira/', 'description':'Charla de Mujeres Pro y creación de Role Models'},
             {'title':'Iniciativa Incentiva', 'type':'is-warning' ,'image':'ninastic.jpg', 'path':'/incentiva/','description':'Talleres Introductorios de Programación'},
             {'title':'Iniciativa Potencia', 'type':'is-success', 'image':'sororidad.jpg', 'path':'/potencia/', 'description':'Cursos Avanzados de Programación'},
         ],
         information:[
-            {'type':'left', 'image':'inspired.jpg', 'text':'Si tienes menos de 18 años. Diviértete, aprende y conecta con otras Niñas Pro como tú.', 'titulo':'¡Participa!', 'boton':'Ver proximas actividades', 'Path':'/voluntariado/'},
-            {'type':'right', 'image':'django-girls.jpg', 'text':'Si quieres ayudarnos. Participa del voluntariado o ayúdanos a llegar a mas niñas a traves de donaciones.', 'titulo':'¡Involúcrate!', 'boton':'Mas información' ,'Path':'/proyectos/'},
+            {'type':'left', 'image':'inspired.jpg', 'text':'Si tienes menos de 18 años. Diviértete, aprende y conecta con otras Niñas Pro como tú.', 'titulo':'¡Participa!', 'boton':'Ver proximas actividades', 'Path':'/eventos/'},
+            {'type':'right', 'image':'django-girls.jpg', 'text':'Si quieres ayudarnos. Participa del voluntariado o ayúdanos a llegar a mas niñas a traves de donaciones.', 'titulo':'¡Involúcrate!', 'boton':'Mas información' ,'Path':'/voluntariado/'},
         ],
         news:[
             {'title':'Titulo 1', 'type':'is-info', 'text':'Ver noticia completa', 'path':'/enconstruccion/'},
