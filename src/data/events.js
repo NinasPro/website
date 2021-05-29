6b91e205a2a3bcc82d06b6a91c4b969f279af9fb
Jazmine Maldonado <jazmine@imfd.com>
2021-05-29 12:02:39 -0400

    change transformando mentes  button state

--- src/data/events.js
+++ src/data/events.js
@@ -3,23 +3,6 @@
 export default ({
     "en":{
         events: [
-            { 
-                'date': '2021-04-10 10:00',
-                'event': 'Curso Anual 2021',
-                'image':'ninastic.jpg',
-                'information': "Curso Anual de programación competitiva en C++",
-                'locale':'Online',
-                'link':'http://eepurl.com/gn7I3r'
-            },
-            { 
-                'date': '2021-05-13 17:30',
-                'event': 'Transformando mentes de niñas y adolescentes',
-                'image':'ninastic.jpg',
-                'information': "En esta actividad, que preparamos junto a otras organizaciones con enfoque de género, \
-                podrás conocer la historia de mujeres referentes en diferentes áreas de conocimiento y compartir con \
-                otras niñas y mujeres. Para participar tienes que identificarte como mujer y tener entre 12 y 18 años.",
-                'locale':'Online','link':''
-            },
         ]
     },
     "es":{
@@ -41,7 +24,7 @@ export default ({
                 otras niñas y mujeres. Para participar tienes que identificarte como mujer y tener entre 12 y 18 años.",
                 'locale':'Online',
                 'link':'https://bit.ly/EncuentroTransformandoMentes',
-                'status':'published'
+                'status':'completed'
             },
             { 
                 'date': '2021-06-05 11:00',
