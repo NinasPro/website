f45bf2b8617223012d4b774518f7d45af96432e5
Pillippa Perez Pons <ignacia.pp@gmail.com>
2025-05-25 20:30:54 -0700

    removed memoria document

--- src/data/about.js
+++ src/data/about.js
@@ -1,14 +1,14 @@
 /* This section corresponding to the data of About view  */
 
 export default ({
-    "en":{
-        memory : [
-            {'title':'Corporative Memory 2019','url':'./Memorias/MEMORIA-2019.pdf'},
+    "en": {
+        memory: [
+            { 'title': 'Corporative Memory 2019', 'url': 'https://8e3f5eb2206d7f2ee947a70b1dc8435a.r2.cloudflarestorage.com/website/MEMORIA-2019.pdf' },
         ]
     },
-    "es":{
-        memory : [
-            {'title':'Memoria Corporativa 2019','url':'./Memorias/MEMORIA-2019.pdf'},
+    "es": {
+        memory: [
+            { 'title': 'Memoria Corporativa 2019', 'url': 'https://8e3f5eb2206d7f2ee947a70b1dc8435a.r2.cloudflarestorage.com/website/MEMORIA-2019.pdf' },
         ]
     }
 })
